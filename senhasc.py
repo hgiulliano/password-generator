677708f8d37c4b8c47455a069271c47daa9c005c
hgiulliano <henrique.giulliano@gmail.com>
2024-06-19 18:38:58 -0300

    modify variables to english

--- senhasc.py
+++ senhasc.py
@@ -1,14 +1,14 @@
 import secrets
 quant = int(input('Quantas senhas você quer? : '))
-tamanho = int(input('De quantos digitos você quer sua senha? (RECOMENDADO: Mais que 8 dígitos):'))
-senhas = []
+size = int(input('De quantos digitos você quer sua senha? (RECOMENDADO: Mais que 8 dígitos):'))
+passwords = []
 #laço de repetição enquanto isso faça isso
 for i in range(quant):# ele faz até chegar no número que você definiu dentro do range
-    senha = secrets.token_urlsafe(tamanho)[:tamanho]
-    senhas.append(senha)#adiciona as senhas na lista senhas
-resultado = '\n'.join(senhas)
+    password = secrets.token_urlsafe(size)[:size]
+    passwords.append(password)#adiciona as senhas na lista senhas
+results= '\n'.join(passwords)
 save = open(r'C:\\Users\\henri\\OneDrive\\Documentos\\pograma\\paito\\projetos\\senhas\\senhas.txt', 'w')
-save.write(f'{resultado}\n')
+save.write(f'{results}\n')
 print('Suas senhas foram armazenadas no arquivo senhas.txt')
 input('PRESSIONE QUALQUER TECLA PRA FINALIZAR O PROGRAMA!')
 
